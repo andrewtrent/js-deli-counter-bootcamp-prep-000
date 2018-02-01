3153609093e91d91fdacdb0cddee57629857fd81
Andrew Trent <andrewtrentdesign@gmail.com>
2018-02-01 19:15:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //var katzDeliLine = []
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
-  
+  var index = katzDeliLine.indexOf(name)
   var pos = parseInt(index) + 1
   return `Welcome, ${name}. You are number ${pos} in line.`
 }
@@ -11,8 +11,7 @@ function nowServing(katzDeliLine){
   if(katzDeliLine.length === 0){
     return `There is nobody waiting to be served!`
     } else{
-    var shiftName = katzDeliLine[0];
-    katzDeliLine.shift();
+    var shiftName = katzDeliLine.shift();
     return `Currently serving ${shiftName}.`;
     
   }
@@ -24,11 +23,11 @@ function currentLine(katzDeliLine){
   } else{
     var i = 0
     var str = "The line is currently: " 
-    while(i+1<line.length){
+    while(i + 1<line.length){
       str = str + `${i+1}. ${katzDeliLine[i]}, `
       i++
     }
-    while(i+1===line.length){
+    while(i + 1===line.length){
       str = str + `${i+1}. ${katzDeliLine[i]}`
     }
       return str
